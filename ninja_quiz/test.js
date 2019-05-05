5b70e5f3f674c514f4403513ea92773a25a277d0
Stanley Futch <30390058+Russ1191@users.noreply.github.com>
2019-05-04 21:21:53 -0400

    Add files via upload

---
+++ ninja_quiz/test.js
@@ -0,0 +1,52 @@
+const correctAnswers = ['B', 'B', 'B', 'B'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+
+form.addEventListener('submit', e =>{
+e.preventDefault();
+
+let score = 0;
+const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+//check answers
+userAnswers.forEach((answer , index) => {
+     if(correctAnswers[index]){
+            score += 25;
+        }
+
+      });
+      
+      scrollTo(0,0)
+      //x and y to go to the top of the page
+      result.classList.remove('d-none');
+
+
+      //make it the number move
+      let output = 0;
+      const timer = setInterval(() =>{
+        result.querySelector('span').textContent =`${output}%`;
+        if(output===score){
+          clearInterval(timer);
+        }else{
+         output++;   
+        }
+     
+      },10);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
